7e1fb2a5d89251130e3a40a866fc37210e6981c1
Wesley-Key <79080491+Wesley-Key@users.noreply.github.com>
2021-07-08 08:43:16 +1200

    Add files via upload

--- primary_maths_interface.py
+++ primary_maths_interface.py
@@ -45,7 +45,28 @@ class Interface:
         Label(self.main, font="Arial 15 bold", text="Year Level: ",bg='#add8e6').grid(row=4,column=2,sticky=W)
         self.yl = Entry(self.main)
         self.yl.grid(row=4, column=2, sticky=E)
-       
+        
+        # Difficulty
+        # Drop down box for the different difficulty types
+        # Create Tkinter Variable
+        self.tkvar = StringVar(root)
+        # Set options
+        self.choices = ['Level 1', 'Level 2', 'Level 3']
+        self.tkvar.set('Choose')
+        
+        self.dropdown = OptionMenu(self.main, self.tkvar, *self.choices)
+        Label(self.main, font="Arial 15 bold", text="Difficulty: ",bg='#add8e6').grid(row=5, column=2, sticky=W)
+        self.dif = self.dropdown.grid(row=5, column=2, sticky=E)
+        
+        def change_dropdown(*args):
+            print(self.tkvar.get())
+        
+        self.tkvar.trace('w', change_dropdown)
+        
+        # Next Button
+        Label(self.main, text="         ",bg='#add8e6').grid(column=1,row=7, pady=50)
+        next_btn = Button(self.main,text="Next",fg="black",highlightbackground="#4cbb17",font="arial 14 bold",height="2", width="10")
+        next_btn.grid(row=7,column=3, padx=10)
         
 
         
