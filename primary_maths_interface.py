590d7bca6743c355f0951489593aa86d883bff46
Wesley-Key <79080491+Wesley-Key@users.noreply.github.com>
2021-08-09 09:42:48 +1200

    Add files via upload

--- primary_maths_interface.py
+++ primary_maths_interface.py
@@ -76,15 +76,15 @@ class Interface:
         self.tkvar.trace('w', change_dropdown)
         
         # Next Button
-        next_btn = Button(self.main,text="Next",fg="black",highlightbackground="#4cbb17",font="arial 14 bold",height="2",width="10",command=self.questions_win)
-        next_btn.grid(row=7,column=3, padx=20, pady=50)
+        self.save_btn = Button(self.main,text="Save",fg="black",highlightbackground="#007cbe",font="arial 14 bold",height="2",width="10",command=self.check_input)
+        self.save_btn.grid(row=7,column=3, padx=20, pady=50)
     
         # Creating instance of students class with users input
         self.student = Student(self.stn, self.yl, self.tkvar) 
         self.range = self.student.range()
         self.year = self.student.year_l()   
     
-    def questions_win(self):
+    def check_input(self):
         # Get users input
         self.stname = (self.stn.get().capitalize())
         self.ylvl = self.yl.get()
@@ -112,42 +112,51 @@ class Interface:
             self.notvalid = False
             
             # Destroying widgets from first window and replacing with new
-            for widget in self.main.winfo_children():
-                widget.destroy()
-                
-            # New widgets (labels, entry boxes and buttons)
-            # Title
-            self.title = Label(self.main, font="Arial 28 bold", text=f"{self.method}", fg="black", bg="#add8e6")
-            self.title.grid(row=0, column=2, pady=20, padx=(30,30))
-            Label(self.main, text="             ",bg='#add8e6').grid(column=1,row=2, padx=50)
+            self.save_btn.destroy()
             
-            # randomising numbers for questions
-            self.x = random.choice(self.range)
-            self.y = random.choice(self.range)
+            self.next = Button(self.main,text="Next",fg="black",highlightbackground="#4cbb17",font="arial 14 bold",height="2",width="10",command=self.questions_win)
+            self.next.grid(row=7,column=3, padx=20, pady=50)
+        
+    def questions_win(self):
+        # destroy all widgets in first frame
+        for widget in self.main.winfo_children():
+            widget.destroy()
+
+        # New widgets (labels, entry boxes and buttons)
+        # Title
+        self.title = Label(self.main, font="Arial 28 bold", text=f"{self.method}", fg="black", bg="#add8e6")
+        self.title.grid(row=0, column=2, pady=20, padx=(30,30))
+        Label(self.main, text="             ",bg='#add8e6').grid(column=1,row=2, padx=50)
+        
+        # set a sign to the method
+        if self.method == "     Addition    ":
+            self.sign = "+"
+        elif self.method == " Subtraction  ":
+            self.sign = "-"
+        elif self.method == "Multiplication":
+            self.sign = "x"
+        else:
+            self.sign = "÷"
             
-            if self.method == "     Addition    ":
-                self.sign = "+"
-            elif self.method == " Subtraction  ":
-                self.sign = "-"
-            elif self.method == "Multiplication":
-                self.sign = "x"
-            else:
-                self.sign = "÷"
-            
-            Label(self.main, font="arial 30", text=f"{self.x}{self.sign}{self.y}", bg="#add8e6").grid(column=2, row=2,pady=(20,0))
-            self.question = Entry(self.main, width=12,bg="#007cbe", fg="white", justify="center", font="arial 20")
-            self.question.grid(column=2, row=3,pady=(20,0))
-            
-            # Check Button 
-            self.check_btn = Button(self.main,text="Check ✓",fg="#ffd639",highlightbackground="#ffd639",font="arial 14 bold",height="2", width="10",command=lambda: self.checkb(self.question))
-            self.check_btn.grid(row=4,column=3, padx=(50,40), pady=(35,60))
-            
-            self.back_btn = Button(self.main,text="⬅ Back",fg="#ed1c24",highlightbackground="#ed1c24",font="arial 14 bold",height="2", width="10")
-            self.back_btn.grid(column=1, row=4, pady=(35,60))
-    
+        # randomising numbers for questions
+        self.x = random.choice(self.range)
+        self.y = random.choice(self.range)
+        
+        # Display question and entry box
+        Label(self.main, font="arial 30", text=f"{self.x}{self.sign}{self.y}", bg="#add8e6").grid(column=2, row=2,pady=(20,0))
+        self.question = Entry(self.main, width=12,bg="#007cbe", fg="white", justify="center", font="arial 20")
+        self.question.grid(column=2, row=3,pady=(20,0))
+        
+        # Check Button 
+        self.check_btn = Button(self.main,text="Check ✓",fg="#ffd639",highlightbackground="#ffd639",font="arial 14 bold",height="2", width="10",command=lambda: self.checkb(self.question))
+        self.check_btn.grid(row=4,column=3, padx=(50,40), pady=(35,60))
+        
+        self.back_btn = Button(self.main,text="⬅ Back",fg="#ed1c24",highlightbackground="#ed1c24",font="arial 14 bold",height="2", width="10")
+        self.back_btn.grid(column=1, row=4, pady=(35,60))
+        
     # Function for the check button, return correct or wrong label
     def checkb(self, var1):
-        self.score = 0
+        self.score=0
         if var1.get() == str(self.correct_answer()):
             correct = Label(self.main, text="✓", fg="green", font="arial 40 bold", bg="#add8e6")
             correct.grid(column=3, row=3, sticky=W)
@@ -161,12 +170,13 @@ class Interface:
             self.check_btn.destroy()
             
             # Replace with next button
-            self.next_btn = Button(self.main,text="Next ⮕",fg="black",highlightbackground="#4cbb17",font="arial 14 bold",height="2", width="10")
+            self.next_btn = Button(self.main,text="Next ⮕",fg="black",highlightbackground="#4cbb17",font="arial 14 bold",height="2", width="10", command=self.questions_win)
             self.next_btn.grid(row=4,column=3, padx=(50,40), pady=(35,60))
             
         else:
             wrong = Label(self.main, text="✘", fg="red", font="arial 40 bold", bg="#add8e6")
             wrong.grid(column=3, row=3, sticky=W)
+            self.score -= 1
     
     # Function to return correct answer
     def correct_answer(self):
@@ -183,14 +193,7 @@ class Interface:
         
     # Quit program function 
     def quit(self):
-        self.main_window.destroy()
-        
-            
-            
-        
-        
-                
-        
+        self.main_window.destroy()       
 
 Interface(root).welcome_frame()
 root.mainloop()
